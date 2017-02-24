f65d6a9a24922be18334326d41d1f85165a8bc82
Skylsmoi <come.huguies@gmail.com>
2017-02-24 13:41:46 +0100

    added parameters to sidebarleft to give param to api call + added param nodeAreLink

--- sidebarleft.jsx
+++ sidebarleft.jsx
@@ -7,17 +7,21 @@ class SidebarLeft extends React.Component {
   constructor () {
     super()
     this.state = {
-      menuTree: []
+      menuTree: [],
+      selectedNode: '' // node selected (clicked) by the user
     }
   }
 
   static propTypes = {
+    nodeAreLink: PropTypes.bool.isRequired,
     apiPath: PropTypes.string.isRequired,
-    apiParameters: PropTypes.string
+    apiParameters: PropTypes.string.isRequired,
+    apiChildPath: PropTypes.string.isRequired,
+    apiChildParameters: PropTypes.string.isRequired
   }
 
   componentDidMount = () => {
-    fetch(this.props.apiPath + 'workspaces/treeview_root' + this.props.apiParameters, {
+    fetch(this.props.apiPath + this.props.apiParameters, {
       method: 'GET',
       headers: { 'Accept': 'application/json' },
       credentials: 'include'
@@ -26,11 +30,26 @@ class SidebarLeft extends React.Component {
     .then(json => this.setState({menuTree: json.d}))
   }
 
+  handleChangeSelectedNode = (newSelectedNodeId) => {
+    this.setState({...this.state, selectedNode: newSelectedNodeId})
+  }
+
   render () {
     return (
       <div className={'sidebarleft textMenuColor'}>
         <div className={'sidebarleft__menu'}>
-          { this.state.menuTree.map((treeItem, i) => <MenuNode data={treeItem} nodeDeepness={0} key={'root_' + i} apiPath={this.props.apiPath} />)}
+          { this.state.menuTree.map((treeItem, i) =>
+            <MenuNode
+              data={treeItem}
+              nodeAreLink={this.props.nodeAreLink}
+              nodeDeepness={0}
+              key={'root_' + i}
+              apiChildPath={this.props.apiChildPath}
+              apiChildParameters={this.props.apiChildParameters || ''}
+              selectedNode={this.state.selectedNode}
+              handlerSelectNode={this.handleChangeSelectedNode}
+            />
+          )}
         </div>
       </div>
     )
@@ -49,7 +68,9 @@ class MenuNode extends React.Component {
   static propTypes = {
     data: PropTypes.object.isRequired,
     nodeDeepness: PropTypes.number.isRequired,
-    apiPath: PropTypes.string.isRequired
+    apiChildPath: PropTypes.string.isRequired,
+    apiChildParameters: PropTypes.string.isRequired,
+    selectedNode: PropTypes.string
   }
 
   handlerOnClickExpandPicto = (e) => {
@@ -66,7 +87,11 @@ class MenuNode extends React.Component {
       return this.setState({...this.state, nodeData: {...nodeData, state: {...nodeData.state, opened: true}}})
     }
 
-    fetch(this.props.apiPath + 'workspaces/treeview_children?id=' + nodeData.id, {
+    const getChildTreeUrl = this.props.apiChildPath + '?id=' + nodeData.id + (this.props.apiChildParameters !== ''
+      ? '&' + this.props.apiChildParameters.substr(1) // substr(1) removes the first '?'
+      : ''
+    )
+    fetch(getChildTreeUrl, {
       method: 'GET',
       headers: { 'Accept': 'application/json' },
       credentials: 'include'
@@ -79,7 +104,7 @@ class MenuNode extends React.Component {
   handleMouseLeave = () => this.setState({...this.state, isNodeHovered: false})
 
   render () {
-    const { nodeDeepness } = this.props
+    const { nodeAreLink, apiChildPath, apiChildParameters, nodeDeepness, selectedNode, handlerSelectNode } = this.props
     const { nodeData, isNodeHovered } = this.state
 
     const styles = {
@@ -89,7 +114,9 @@ class MenuNode extends React.Component {
     }
 
     const nodeClass = (rez => {
-      if (nodeData.state.selected) rez += 'textMenuClickedColor textMenuClickedBgColor '
+      // displays node as selected, in sidebarleft, only when they have the selected attribute at true
+      // in the move folder popup, only when it is the clicked node
+      if ((nodeAreLink && nodeData.state.selected) || selectedNode === nodeData.id) rez += 'textMenuClickedColor textMenuClickedBgColor '
       if (isNodeHovered) rez += 'textMenuColor-hover textMenuBgColor-hover '
       return rez
     })('sidebarleft__menu__item ')
@@ -113,19 +140,24 @@ class MenuNode extends React.Component {
             <div className={'sidebarleft__menu__item__expandpicto'} onClick={e => this.handlerOnClickExpandPicto(e)}>
               <i className={'fa ' + expandPicto} />
             </div>
-            <a className={'sidebarleft__menu__item__link'} href={nodeData.a_attr.href}>
-              <i className={'fa ' + faIcon} />
-              { nodeData.text }
-            </a>
+            { nodeAreLink
+                ? <a className={'sidebarleft__menu__item__link'} href={nodeData.a_attr.href} id={nodeData.id}><i className={'fa ' + faIcon} />{ nodeData.text }</a>
+                : <div className={'sidebarleft__menu__item__link'} id={nodeData.id} onClick={() => handlerSelectNode(nodeData.id)}> <i className={'fa ' + faIcon} />{ nodeData.text }</div>
+            }
+
           </div>
         </div>
         <Collapse isOpened={Array.isArray(nodeData.children)} springConfig={{stiffness: 500, damping: 40}} hasNestedCollapse>
           { Array.isArray(nodeData.children) && nodeData.children.map((child, i) =>
             <MenuNode
               data={child}
+              nodeAreLink={nodeAreLink}
               nodeDeepness={nodeDeepness + 1}
               key={nodeDeepness + '_' + i}
-              apiPath={this.props.apiPath}
+              apiChildPath={apiChildPath}
+              apiChildParameters={apiChildParameters}
+              selectedNode={selectedNode}
+              handlerSelectNode={handlerSelectNode}
             />
           )}
         </Collapse>
@@ -135,7 +167,9 @@ class MenuNode extends React.Component {
 }
 
 // wrapper for app launcher (it's better to have the wrapper in a separated file but since this app can only be used for tracim, it's not required)
-const sidebarLeft = (element, apiPath, apiParameters = '') => {
-  ReactDOM.render(<SidebarLeft apiPath={apiPath} apiParameters={apiParameters} />, element)
+const sidebarLeft = (element, nodeAreLink, apiPath, apiParameters = '', apiChildPath = '', apiChildParameters = '') => {
+  ReactDOM.render(
+    <SidebarLeft nodeAreLink={nodeAreLink} apiPath={apiPath} apiParameters={apiParameters} apiChildPath={apiChildPath} apiChildParameters={apiChildParameters} />, element
+  )
 }
 module.exports = sidebarLeft
